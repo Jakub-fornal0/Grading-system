1d95f8c9050361c8567b9cbb07ce4ac69adc2b3f
Jakub Fornal <jakub.fornal0@onet.pl>
2025-05-21 07:24:31 +0200

    feat: added sidebar with navigation

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { TestComponent } from './layout/test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'configuration', pathMatch: 'full' },
+      { path: 'home', component: TestComponent },
+      { path: 'configuration', component: TestComponent },
+    ],
+  },
+];
---
+++ client/src/app/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'layout',
+  standalone: true,
+  imports: [SidebarComponent, RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {}
